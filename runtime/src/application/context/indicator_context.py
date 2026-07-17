084e4015f673afab17e4bdf85e38f025a6c60187
bigJoseph521 <fergusonjoseph521@gmail.com>
2026-07-17 04:41:44 -0400

    add indicator class name in log

--- runtime/src/application/context/indicator_context.py
+++ runtime/src/application/context/indicator_context.py
@@ -552,7 +552,7 @@ class RuntimeIndicatorContext(IndicatorContext):
             
             self._logger.platform_info(
                 message="Indicator calculations finished",
-                indicator= registered.handle.key,
+                indicator= registered.indicator.__class__.__name__,
                 symbol=symbol,
                 timeframe=timeframe,
                 window= registered.indicator.required_history,
